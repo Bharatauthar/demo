4e76064b7452f14ae5b748c67ee0d0f87976ed2b
Bharatauthar <132898066+Bharatauthar@users.noreply.github.com>
2023-05-25 22:29:44 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 var app=require("express")()
 var port=process.env.port || 8000
+
+app.set('view engine','hbs')
 app.get('/',(req,res)=>
 {
-    res.end("bharat")
+    res.render('abc')
 })
 app.listen(port)
